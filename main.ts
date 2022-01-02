7093ed8d01b7c693e215f6aa9c48b737bd6bbe9e
Alexander H <54555500+cheeseonamonkey@users.noreply.github.com>
2022-01-02 04:16:08 -0700

    comet

--- main.ts
+++ main.ts
@@ -1,5 +1,4 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
-
+import { App, Editor, MarkdownView, Modal, addIcon, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
 
 
 
@@ -19,9 +18,16 @@ export default class MyPlugin extends Plugin {
 	async onload() {
 		await this.loadSettings();
 		
+		addIcon("mykeyicon",
+		`<path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+		<circle cx="8" cy="15" r="4" />
+		<line x1="10.85" y1="12.15" x2="19" y2="4" />
+		<line x1="18" y1="5" x2="20" y2="7" />
+		<line x1="15" y1="8" x2="17" y2="10" />`);
+
 		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('key',
-		'Sample Plugin',
+		const ribbonIconEl = this.addRibbonIcon('mykeyicon',
+		'Sample Plugin', 
 		(evt: MouseEvent) =>
 		{
 			//this is called when the user clicks the icon
